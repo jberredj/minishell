e7efcd117035da4b29b03143b16663277de30236
Daria DIAKOVA <ddiakova@e2r2p19.clusters.42paris.fr>
2021-11-14 21:01:43 +0100

    echo with write

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -3,16 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ddiakova <ddiakova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 11:23:55 by jberredj          #+#    #+#             */
-/*   Updated: 2021/11/02 11:36:37 by jberredj         ###   ########.fr       */
+/*   Updated: 2021/11/14 20:46:32 by ddiakova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "../../libft/includes/ft_string.h"
 
 int	echo(char **argv)
 {
-	write(1)
+	int	i;
+
+	i = 0;
+	while (argv[i])
+		i++;
+	if (i == 1)
+	{
+		write(1, " ", 1);
+		write (1, "\n", 2);
+	}
+	else
+	{	
+		i = 1;
+		while (argv[i])
+		{
+			write(1, &argv[i], ft_strlen(argv[i]));
+			write(1, " ", 1);
+			i++;
+		}
+		write (1, "\n", 2);
+	}
+	return (0);
 }
